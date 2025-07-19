43bf4ccf23500f9fc6d317ce5509904a168ed599
Aryan Regmi <aryan_regmi@protonmail.com>
2025-07-19 14:12:47 -0600

    Changed to message passing instead of direct function calling

--- native/src/messages.rs
+++ native/src/messages.rs
@@ -5,7 +5,9 @@ use std::{
 
 use bincode::{Decode, Encode, decode_from_slice, encode_into_slice};
 
-use crate::state::load_model;
+use crate::state::{
+    WakeWordDetection, detect_wake_words, load_model, transcribe, update_audio_data,
+};
 
 /// Represents messages sent **from** Dart **to** Rust.
 #[derive(Debug, Encode, Decode)]
@@ -15,6 +17,14 @@ pub enum Message {
 
     /// Update the audio data to be transcribed.
     UpdateAudioData(Vec<f32>),
+
+    // FIXME: Create separate message for setting wake words
+    //
+    /// Detect for the given wake words in the audio data.
+    DetectWakeWords(Vec<String>),
+
+    /// Transcribes the audio data sent through `Message::UpdateAudioData`.
+    Transcribe,
 }
 
 /// Represents the response **from** Rust **to** a Dart message.
@@ -24,7 +34,7 @@ pub struct Response {
     status: ResponseStatus,
 
     /// Any additional details associated with the response.
-    details: String,
+    details: ResponseDetails,
 }
 
 /// The status of the response from Rust.
@@ -34,18 +44,25 @@ pub enum ResponseStatus {
     Error,
 }
 
-// FIXME: Add `res_len: *mut usize` output param, so responses can be freed correctly!
-// - Update `rust_error` to also change this length
-//
+#[derive(Debug, Encode, Decode)]
+pub enum ResponseDetails {
+    Text(String),
+    WakeWordDetected(WakeWordDetection),
+}
+
 /// Sends the specified message *to* Rust *from* Dart.
 ///
 /// This function will return return a `Response` pointer that must be deallocated using the
 /// [free_response] function.
 #[unsafe(no_mangle)]
-pub fn send_message_to_rust(msg_ptr: *const ffi::c_void, msg_len: usize) -> *mut ffi::c_void {
+pub fn send_message_to_rust(
+    msg_ptr: *const ffi::c_void,
+    msg_len: usize,
+    resp_len: *mut usize,
+) -> *mut ffi::c_void {
     // Decode message
     let message: Message = deserialize(msg_ptr, msg_len)
-        .map_err(|e| return rust_error(e))
+        .map_err(|e| return rust_error(e, resp_len))
         .unwrap();
 
     // Respond to message
@@ -53,23 +70,51 @@ pub fn send_message_to_rust(msg_ptr: *const ffi::c_void, msg_len: usize) -> *mut
         Message::LoadModel(model_path) => {
             // Loads the model
             load_model(&model_path)
-                .map_err(|e| return rust_error(e))
+                .map_err(|e| return rust_error(e, resp_len))
                 .unwrap();
             Response {
                 status: ResponseStatus::Success,
-                details: format!("Model path set to: {model_path}"),
+                details: ResponseDetails::Text(format!("Model path set to: {model_path}")),
             }
         }
-        Message::UpdateAudioData(items) => {
-            // FIXME: update audio data
+        Message::UpdateAudioData(new_data) => {
+            // Updates the audio data
+            update_audio_data(&new_data)
+                .map_err(|e| return rust_error(e, resp_len))
+                .unwrap();
             Response {
                 status: ResponseStatus::Success,
-                details: format!("Audio data updated with {} samples", items.len()),
+                details: ResponseDetails::Text(format!(
+                    "Audio data updated with {} samples",
+                    new_data.len()
+                )),
+            }
+        }
+        Message::DetectWakeWords(wake_words) => {
+            // Detects the wake word
+            let detection = detect_wake_words(&wake_words)
+                .map_err(|e| return rust_error(e, resp_len))
+                .unwrap();
+            Response {
+                status: ResponseStatus::Success,
+                details: ResponseDetails::WakeWordDetected(detection),
+            }
+        }
+        Message::Transcribe => {
+            // Transcribes the audio data
+            let transcript = transcribe()
+                .map_err(|e| return rust_error(e, resp_len))
+                .unwrap();
+            Response {
+                status: ResponseStatus::Success,
+                details: ResponseDetails::Text(transcript),
             }
         }
     };
-    serialize(response)
-        .map_err(|e| return rust_error(e))
+
+    // Return serialized response
+    serialize(response, resp_len)
+        .map_err(|e| return rust_error(e, resp_len))
         .unwrap()
 }
 
@@ -89,11 +134,12 @@ pub fn free_response(ptr: *const ffi::c_void, len: usize) {
 ///
 /// # Note
 /// The callier must free the the returned pointer with [free_response].
-fn serialize<T: Encode>(value: T) -> Result<*mut ffi::c_void, String> {
+fn serialize<T: Encode>(value: T, len_ptr: *mut usize) -> Result<*mut ffi::c_void, String> {
     let mut bytes = Vec::<u8>::with_capacity(size_of_val(&value));
-    encode_into_slice(value, bytes.as_mut_slice(), bincode::config::standard())
+    let written = encode_into_slice(value, bytes.as_mut_slice(), bincode::config::standard())
         .map_err(|e| e.to_string())?;
     let response_ptr: *mut ffi::c_void = Box::into_raw(bytes.into_boxed_slice()).cast();
+    unsafe { *len_ptr = written };
     Ok(response_ptr)
 }
 
@@ -115,10 +161,10 @@ fn deserialize<T: Decode<()>>(ptr: *const ffi::c_void, len: usize) -> Result<T, 
 }
 
 /// Returns an error `Response`.
-fn rust_error(details: String) -> *mut ffi::c_void {
+fn rust_error(details: String, resp_len: *mut usize) -> *mut ffi::c_void {
     let error = Response {
         status: ResponseStatus::Error,
-        details,
+        details: ResponseDetails::Text(details),
     };
-    return serialize(error).unwrap();
+    serialize(error, resp_len).unwrap()
 }
--- native/src/state.rs
+++ native/src/state.rs
@@ -2,21 +2,20 @@
 
 use std::sync::{LazyLock, Mutex};
 
-use whisper_rs::{WhisperContext, WhisperContextParameters, WhisperState};
+use bincode::{Decode, Encode};
+use whisper_rs::{
+    FullParams, SamplingStrategy, WhisperContext, WhisperContextParameters, WhisperState,
+};
 
 /// The expected sample rate for the audio data.
 const EXPECTED_SAMPLE_RATE: usize = 44_100;
 
 /// The state of the Whisper model.
-static MODEL_STATE: LazyLock<Mutex<Option<WhisperState>>> = LazyLock::new(|| Mutex::new(None));
+pub static MODEL_STATE: LazyLock<Mutex<Option<WhisperState>>> = LazyLock::new(|| Mutex::new(None));
 
 /// The current audio data to be processed.
-static AUDIO_DATA: LazyLock<Mutex<Vec<f32>>> =
+pub static AUDIO_DATA: LazyLock<Mutex<Vec<f32>>> =
     LazyLock::new(|| Mutex::new(Vec::with_capacity(EXPECTED_SAMPLE_RATE)));
-
-/// The current transcript.
-static TRANSCRIPT: LazyLock<Mutex<String>> =
-    LazyLock::new(|| Mutex::new(String::with_capacity(1024)));
 
 /// Loads the Whisper model from the given path.
 pub fn load_model(model_path: &str) -> Result<(), String> {
@@ -30,4 +29,90 @@ pub fn load_model(model_path: &str) -> Result<(), String> {
     *model = Some(state);
 
     todo!()
+}
+
+/// Updates the audio data to be transcribed.
+pub fn update_audio_data(data: &[f32]) -> Result<(), String> {
+    let mut audio_data = AUDIO_DATA.lock().map_err(|e| e.to_string())?;
+    audio_data.clear();
+    audio_data.extend_from_slice(data);
+    Ok(())
+}
+
+/// Result of wake word detection.
+#[derive(Debug, Encode, Decode)]
+pub struct WakeWordDetection {
+    /// Whether or not a wake word was detected.
+    pub detected: bool,
+
+    /// The start index in the transcript of the detected word.
+    pub start_idx: Option<usize>,
+
+    /// The end index in the transcript of the detected word.
+    pub end_idx: Option<usize>,
+}
+
+/// Checks if any wake words are present in the provided audio data.
+///
+/// # Note
+/// This should only be called after [update_audio_data].
+pub fn detect_wake_words(wake_words: &[String]) -> Result<WakeWordDetection, String> {
+    let audio_data = AUDIO_DATA.lock().map_err(|e| e.to_string())?;
+    if !audio_data.is_empty() {
+        // Get transcript by running model
+        let transcript = run_model(&audio_data)?.to_lowercase();
+
+        // Check for wake words
+        for phrase in wake_words {
+            if let Some(idx) = transcript.find(&phrase.to_lowercase()) {
+                return Ok(WakeWordDetection {
+                    detected: true,
+                    start_idx: Some(idx),
+                    end_idx: Some(phrase.len()),
+                });
+            }
+        }
+    }
+
+    Ok(WakeWordDetection {
+        detected: false,
+        start_idx: None,
+        end_idx: None,
+    })
+}
+
+/// Transcribes the audio data.
+pub fn transcribe() -> Result<String, String> {
+    let audio_data = AUDIO_DATA.lock().map_err(|e| e.to_string())?;
+    if !audio_data.is_empty() {
+        run_model(&audio_data)
+    } else {
+        Ok("".into())
+    }
+}
+
+/// Runs the stored model with the given audio data.
+fn run_model(audio_data: &[f32]) -> Result<String, String> {
+    let mut model = MODEL_STATE.lock().map_err(|e| e.to_string())?;
+
+    if let Some(state) = &mut *model {
+        // Run model
+        state
+            .full(
+                FullParams::new(SamplingStrategy::Greedy { best_of: 1 }),
+                audio_data,
+            )
+            .map_err(|e| e.to_string())?;
+
+        // Save results
+        let mut transcript = String::with_capacity(1026);
+        let num_segments = state.full_n_segments().unwrap();
+        for i in 0..num_segments {
+            let segment = state.full_get_segment_text(i).unwrap();
+            transcript.push_str(&segment);
+        }
+        Ok(transcript)
+    } else {
+        Err("Invalid model state".into())
+    }
 }
